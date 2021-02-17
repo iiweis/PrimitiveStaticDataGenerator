86755d9789ad130788e3037b40b885633f3dedb2
iiweis <iiweis.net@gmail.com>
2021-02-17 23:47:21 +0900

    add SourceGenerator

---
+++ src/PrimitiveStaticDataGenerator/Internal/Util.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimitiveStaticDataGenerator.Internal
+{
+    internal static class Util
+    {
+        internal static void Throw(Exception exception) => throw exception;
+
+        internal static IEnumerable<SyntaxNode> ParentNodes(this SyntaxNode node)
+        {
+            if (node is null) Throw(new ArgumentNullException(nameof(node)));
+
+            return getParentNodes(node!);
+
+            static IEnumerable<SyntaxNode> getParentNodes(SyntaxNode node)
+            {
+                SyntaxNode? parent = node.Parent;
+                do
+                {
+                    if (parent is null) yield break;
+                    yield return parent;
+                    parent = parent.Parent;
+
+                } while (true);
+            }
+        }
+
+        internal static string ToValidHintName(this string hintName)
+        {
+            Span<char> chars = hintName.ToCharArray();
+           
+            for (int i = 0; i < chars.Length; i++)
+            {
+                ref char c = ref chars[i];
+
+                if (!SyntaxFacts.IsIdentifierPartCharacter(c)
+                    && c != '.'
+                    && c != ','
+                    && c != '-'
+                    && c != '_'
+                    && c != ' '
+                    && c != '('
+                    && c != ')'
+                    && c != '['
+                    && c != ']'
+                    && c != '{'
+                    && c != '}')
+                {
+                    c = '_';
+                }
+            }
+
+            return chars.ToString();
+        }
+
+        internal static string GetFullMetadataName(this ISymbol? s)
+        {
+            if (s == null || IsRootNamespace(s)) return string.Empty;
+
+            var sb = new StringBuilder(s.MetadataName);
+
+            s = s.ContainingSymbol;
+            while (!IsRootNamespace(s))
+            {
+                sb.Insert(0, '.');
+                sb.Insert(0, s.OriginalDefinition.MetadataName);
+                s = s.ContainingSymbol;
+            }
+
+            return sb.ToString();
+        }
+
+        internal static bool IsExtendedPartial(this MethodDeclarationSyntax method)
+        {
+            if (method is null) return false;
+
+            var hasAccessibility = false;
+            var isPartial = false;
+
+            for (int i = 0; i < method.Modifiers.Count; ++i)
+            {
+                SyntaxKind kind = method.Modifiers[i].Kind();
+
+                if (!hasAccessibility && SyntaxFacts.IsAccessibilityModifier(kind))
+                {
+                    hasAccessibility = true;
+                }
+                else if(!isPartial && kind == SyntaxKind.PartialKeyword)
+                {
+                    isPartial = true;
+                }
+
+                if (isPartial && hasAccessibility)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        internal static T DescendantNodeAt<T>(this SyntaxNode node, int index) where T : SyntaxNode
+            => node.DescendantNodes().OfType<T>().ElementAt(index);
+
+        internal static bool IsRootNamespace(this ISymbol symbol)
+            => symbol is INamespaceSymbol s && s.IsGlobalNamespace;
+
+        internal static CompilationUnitSyntax ImplementPartial(this MethodDeclarationSyntax method, Func<MethodDeclarationSyntax, MethodDeclarationSyntax> action)
+        {
+            if (method is null) Throw(new ArgumentNullException(nameof(method)));
+
+             TypeDeclarationSyntax? typeDeclaration = null;
+
+            foreach (var type in method!.ParentNodes().OfType<TypeDeclarationSyntax>().Reverse().Select(t => t.WithAttributeLists(default).WithMembers(default)))
+            {
+                if (typeDeclaration is null)
+                {
+                    typeDeclaration = type;
+                }
+                else
+                {
+                    typeDeclaration = typeDeclaration.AddMembers(type);
+                }
+            }
+            if (typeDeclaration is null) Throw(new InvalidOperationException());
+
+            CompilationUnitSyntax compilationUnit = SyntaxFactory.CompilationUnit();
+
+            compilationUnit = compilationUnit.AddUsings(method.SyntaxTree.GetCompilationUnitRoot().Usings.ToArray());
+
+            NamespaceDeclarationSyntax @namespace = method.ParentNodes().OfType<NamespaceDeclarationSyntax>().LastOrDefault();
+
+            MethodDeclarationSyntax methodDeclaration = action(method.WithAttributeLists(default).WithSemicolonToken(default));
+
+            if (typeDeclaration!.DescendantNodes().OfType<TypeDeclarationSyntax>().LastOrDefault() is { } lastType)
+            {
+                typeDeclaration = typeDeclaration.ReplaceNode(lastType, lastType.AddMembers(methodDeclaration));
+            }
+            else
+            {
+                typeDeclaration = typeDeclaration.AddMembers(methodDeclaration);
+            }
+
+            if (@namespace is null)
+            {
+                compilationUnit = compilationUnit.AddMembers(typeDeclaration);
+            }
+            else
+            {
+                @namespace = @namespace.AddMembers(typeDeclaration);
+                compilationUnit = compilationUnit.AddMembers(@namespace);
+            }
+
+            return compilationUnit;
+        }
+
+    }
+}
---
+++ src/PrimitiveStaticDataGenerator/SourceGenerator.cs
@@ -0,0 +1,335 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using PrimitiveStaticDataGenerator.Internal;
+using System;
+using System.Buffers.Binary;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace PrimitiveStaticDataGenerator
+{
+    [Generator]
+    class SourceGenerator : ISourceGenerator
+    {
+
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+#if DEBUG
+            //if (!Debugger.IsAttached) Debugger.Launch();
+#endif
+            if (!(context.Compilation is CSharpCompilation compilation)) return;
+            if (!(context.ParseOptions is CSharpParseOptions parseOptions)) return;
+          
+            // check if required types exists.
+            if (!(compilation.GetTypeByMetadataName("System.ReadOnlySpan`1") is { } readOnlySpanSymbol)) return;
+            if (!(compilation.GetTypeByMetadataName("System.Runtime.CompilerServices.Unsafe") is { } unsafeSymbol)) return;
+            if (!(compilation.GetTypeByMetadataName("System.Runtime.InteropServices.MemoryMarshal") is { } memoryMarshalSymbol)) return;
+
+            SourceText attributeSourceText = constractSourceText(new PrimitiveStaticDataAttributeTemplate().TransformText());
+            context.AddSource(PrimitiveStaticDataAttributeTemplate.TypeFullName, attributeSourceText);
+
+            try
+            {
+                compilation = compilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(attributeSourceText, options: parseOptions));
+
+                INamedTypeSymbol attrSymbol = compilation.GetTypeByMetadataName(PrimitiveStaticDataAttributeTemplate.TypeFullName)!;
+
+                var candidateMethodSyntaxes = compilation.
+                    SyntaxTrees.
+                    SelectMany(tree => tree.GetRoot().DescendantNodes()).
+                    OfType<MethodDeclarationSyntax>().
+                    Where(method => method.IsExtendedPartial()
+                        && method.Modifiers.Any(SyntaxKind.StaticKeyword)
+                        && method.AttributeLists.Count > 0
+                        && method.ParentNodes().OfType<TypeDeclarationSyntax>().All(type => type.Modifiers.Any(SyntaxKind.PartialKeyword)));
+
+                foreach (var methodSyntax in candidateMethodSyntaxes)
+                {
+                    SemanticModel semantic = compilation.GetSemanticModel(methodSyntax.SyntaxTree);
+
+                    if (!(semantic.GetDeclaredSymbol(methodSyntax) is { } methodSymbol)
+                        || methodSymbol.MethodKind != MethodKind.Ordinary
+                        || methodSymbol.PartialImplementationPart is not null
+                        || !equalsSymbol(methodSymbol.ReturnType.OriginalDefinition, readOnlySpanSymbol)) continue;
+
+                    var returnSpanType = (INamedTypeSymbol)methodSymbol.ReturnType;
+                    if (!isPrimitiveType(returnSpanType.TypeArguments[0])) continue;
+
+                    foreach (AttributeData attr in methodSymbol.GetReturnTypeAttributes())
+                    {
+                        if (!equalsSymbol(attr.AttributeClass, attrSymbol)
+                            || attr.ConstructorArguments.Length < 1) continue;
+
+                        TypedConstant arg1 = attr.ConstructorArguments[0];
+                        if (arg1.Kind != TypedConstantKind.Array || arg1.IsNull) continue;
+
+                        var arraySymbol = (IArrayTypeSymbol)arg1.Type!;
+                        if (arg1.Values.Length == 0
+                            || !arg1.Values.All(v => v.Kind == TypedConstantKind.Primitive)
+                            || !equalsSymbol(arraySymbol.ElementType, returnSpanType.TypeArguments[0])) continue;
+
+                        var typeKeyword = arraySymbol.ElementType.ToDisplayString(new SymbolDisplayFormat(
+                                          typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameOnly,
+                                          miscellaneousOptions: SymbolDisplayMiscellaneousOptions.UseSpecialTypes));
+
+                        SourceText generatedSource;
+                        if (arraySymbol.ElementType.SpecialType is SpecialType.System_Boolean or SpecialType.System_SByte or SpecialType.System_Byte)
+                        {
+                            var arrayCreation = (ArrayCreationExpressionSyntax)ParseExpression($"new {typeKeyword}[] {{ {string.Join(", ", arg1.Values.Select(v => v.ToCSharpString()))} }}", options: parseOptions);
+
+                            CompilationUnitSyntax methodImplementation = methodSyntax.ImplementPartial(
+                                m => m.WithExpressionBody(ArrowExpressionClause(arrayCreation)).
+                                     WithSemicolonToken(Token(SyntaxKind.SemicolonToken)));
+
+                            generatedSource = constractSourceText(methodImplementation.NormalizeWhitespace().ToFullString());
+                        }
+                        else
+                        {
+                            const string generateCodeBase =
+@"{
+    ReadOnlySpan<byte> span;
+    if (BitConverter.IsLittleEndian)
+    {
+        span = new byte[] {};
+    }
+    else
+    {
+        span = new byte[] {};
+    }
+    return default;
+}";
+                            object[] values = arg1.Values.Select(v => v.Value!).ToArray();
+
+                            var block = (BlockSyntax)ParseStatement(generateCodeBase, options: parseOptions);
+
+                            var littleEndianValues = SeparatedList<ExpressionSyntax>(
+                                valuesToBytes(arraySymbol.ElementType.SpecialType, values, littleEndian: true).
+                                    Select(b => LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(b))));
+
+                            block = block.ReplaceNode(block.DescendantNodeAt<ArrayCreationExpressionSyntax>(0).Initializer!,
+                                 InitializerExpression(SyntaxKind.ArrayInitializerExpression, littleEndianValues));
+
+                            var bigEndianValues = SeparatedList<ExpressionSyntax>(
+                                valuesToBytes(arraySymbol.ElementType.SpecialType, values, littleEndian: false).
+                                    Select(b => LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(b))));
+
+                            block = block.ReplaceNode(block.DescendantNodeAt<ArrayCreationExpressionSyntax>(1).Initializer!,
+                                   InitializerExpression(SyntaxKind.ArrayInitializerExpression, bigEndianValues));
+
+                            // HACK: no good.
+                            StatementSyntax @return;
+                            if (memoryMarshalSymbol.GetMembers("CreateReadOnlySpan").OfType<IMethodSymbol>().Any())
+                            {
+                                @return = ParseStatement(
+                                    $"return MemoryMarshal.CreateReadOnlySpan(ref Unsafe.As<byte, {typeKeyword}>(ref MemoryMarshal.GetReference(span)), {values.Length});", 
+                                    options: parseOptions);
+                            }
+                            else if (compilation.Options.AllowUnsafe)
+                            {
+                                @return = ParseStatement(
+                                    $"unsafe {{ return new ReadOnlySpan<{typeKeyword}>(Unsafe.AsPointer(ref MemoryMarshal.GetReference(span)), {values.Length}); }}",
+                                    options: parseOptions);
+                            }
+                            else if (memoryMarshalSymbol.GetMembers("Cast").OfType<IMethodSymbol>().Where(m => equalsSymbol(m.ReturnType.OriginalDefinition, readOnlySpanSymbol)).Any())
+                            {
+                                @return = ParseStatement(
+                                    $"return MemoryMarshal.Cast<byte, {typeKeyword}>(span);",
+                                    options: parseOptions);
+                            }
+                            else
+                            {
+                                continue;
+                            }
+
+                            block = block.ReplaceNode(block.DescendantNodeAt<ReturnStatementSyntax>(0), @return);
+
+                            CompilationUnitSyntax methodImplementation = 
+                                Util.ImplementPartial(methodSyntax, m => m.WithBody(block));
+
+                            methodImplementation = methodImplementation.AddUsings(UsingDirective(ParseName(memoryMarshalSymbol.ContainingNamespace.ToDisplayString())));
+                            methodImplementation = methodImplementation.AddUsings(UsingDirective(ParseName(unsafeSymbol.ContainingNamespace.ToDisplayString())));
+
+                            generatedSource = constractSourceText(methodImplementation.NormalizeWhitespace().ToFullString());
+                        }
+
+                        context.AddSource(methodSymbol.GetFullMetadataName().ToValidHintName(), generatedSource);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.ToString());
+            }
+
+            static byte[] valuesToBytes(SpecialType primitiveType, object[] values, bool littleEndian)
+            {
+                int size = getPrimitiveTypeSize(primitiveType);
+                var buffer = new byte[size * values.Length];
+
+                for (int i = 0; i < values.Length; ++i)
+                {
+                    ref var boxValue = ref values[i];
+                    var bufferSpan = ((Span<byte>)buffer).Slice(size * i);
+
+                    switch (primitiveType)
+                    {
+                        case SpecialType.System_Char:
+                            {
+                                ref var charValue = ref Unsafe.Unbox<char>(boxValue);
+                                var value = Unsafe.As<char, ushort>(ref charValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteUInt16LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteUInt16BigEndian(bufferSpan, value);
+                                break;
+                            }
+                        case SpecialType.System_UInt16:
+                            {
+                                ref var value = ref Unsafe.Unbox<ushort>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteUInt16LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteUInt16BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_Int16:
+                            {
+                                ref var value = ref Unsafe.Unbox<short>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteInt16LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteInt16BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_Int32:
+                            {
+                                ref var value = ref Unsafe.Unbox<int>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteInt32LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteInt32BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_UInt32:
+                            {
+                                ref var value = ref Unsafe.Unbox<uint>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteUInt32LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteUInt32BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_Int64:
+                            {
+                                ref var value = ref Unsafe.Unbox<long>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteInt64LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteInt64BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_UInt64:
+                            {
+                                ref var value = ref Unsafe.Unbox<ulong>(boxValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteUInt64LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteUInt64BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_Single:
+                            {
+                                ref var floatValue = ref Unsafe.Unbox<float>(boxValue);
+                                var value = Unsafe.As<float, int>(ref floatValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteInt32LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteInt32BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        case SpecialType.System_Double:
+                            {
+                                ref var doubleValue = ref Unsafe.Unbox<double>(boxValue);
+                                var value = Unsafe.As<double, long>(ref doubleValue);
+                                if (littleEndian)
+                                    BinaryPrimitives.WriteInt64LittleEndian(bufferSpan, value);
+                                else
+                                    BinaryPrimitives.WriteInt64BigEndian(bufferSpan, value);
+                                break;
+                            }
+
+                        default:
+                            throw new InvalidOperationException();
+                    }
+                }
+
+                return buffer;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static bool isPrimitiveType(ITypeSymbol symbol)
+            {
+                switch (symbol.SpecialType)
+                {
+                    case SpecialType.System_Boolean:
+                    case SpecialType.System_Char:
+                    case SpecialType.System_SByte:
+                    case SpecialType.System_Byte:
+                    case SpecialType.System_Int16:
+                    case SpecialType.System_UInt16:
+                    case SpecialType.System_Int32:
+                    case SpecialType.System_UInt32:
+                    case SpecialType.System_Int64:
+                    case SpecialType.System_UInt64:
+                    case SpecialType.System_Single:
+                    case SpecialType.System_Double:
+                        return true;
+                }
+                return false;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static int getPrimitiveTypeSize(SpecialType primitiveType)
+            {
+                return primitiveType switch
+                {
+                    SpecialType.System_Boolean => sizeof(bool),
+                    SpecialType.System_Char => sizeof(char),
+                    SpecialType.System_SByte => sizeof(sbyte),
+                    SpecialType.System_Byte => sizeof(byte),
+                    SpecialType.System_Int16 => sizeof(short),
+                    SpecialType.System_UInt16 => sizeof(ushort),
+                    SpecialType.System_Int32 => sizeof(int),
+                    SpecialType.System_UInt32 => sizeof(uint),
+                    SpecialType.System_Int64 => sizeof(long),
+                    SpecialType.System_UInt64 => sizeof(ulong),
+                    SpecialType.System_Single => sizeof(float),
+                    SpecialType.System_Double => sizeof(double),
+                    _ => throw new ArgumentException(nameof(primitiveType)),
+                };
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static bool equalsSymbol(ISymbol? symbol1, ISymbol? symbol2)
+                => SymbolEqualityComparer.Default.Equals(symbol1, symbol2);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            static SourceText constractSourceText(string text)
+                => SourceText.From(text, Encoding.UTF8);
+        }
+    }
+}
