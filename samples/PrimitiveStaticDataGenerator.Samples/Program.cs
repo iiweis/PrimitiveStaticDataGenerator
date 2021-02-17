739032a85d234658efaf922c6f3ba73f85045338
iiweis <iiweis.net@gmail.com>
2021-02-17 23:48:23 +0900

    add sample

--- samples/PrimitiveStaticDataGenerator.Samples/Program.cs
+++ samples/PrimitiveStaticDataGenerator.Samples/Program.cs
@@ -1,11 +1,85 @@
 ﻿using System;
+using PrimitiveStaticDataGenerator;
+using ReadOnlySpanByte = System.ReadOnlySpan<byte>;
+using AttributeAlias = PrimitiveStaticDataGenerator.PrimitiveStaticDataAttribute;
 
-namespace PrimitiveStaticDataGenerator.Samples
+Root.Write();
+
+static partial class Root
 {
-    class Program
+    public static void Write()
     {
-        static void Main(string[] args)
+        Write<bool>(Bool());
+        Write<byte>(Byte());
+        Write<sbyte>(SByte());
+        Write<char>(Char());
+        Write<short>(Short());
+        Write<ushort>(UShort());
+        ChildStruct.Write();
+        ChildClass<object>.Write();
+    }
+
+    static void Write<T>(ReadOnlySpan<T> span)
+    {
+        Console.Write($"{typeof(T)} ");
+        foreach (var item in span)
+            Console.Write($"{item}, ");
+        Console.WriteLine();
+    }
+
+    [return: AttributeAlias(true, false)]
+    public static partial System.ReadOnlySpan<bool> Bool();
+
+    [return: PrimitiveStaticData(new byte[] { 1, 2, 3, byte.MinValue, byte.MaxValue })]
+    public static partial ReadOnlySpanByte Byte();
+
+    [return: PrimitiveStaticData(new SByte[] { 0, 1, -1, sbyte.MinValue, sbyte.MaxValue })]
+    public static partial ReadOnlySpan<SByte> SByte();
+
+    [return: PrimitiveStaticData('A', 'B', 'C', char.MinValue, char.MaxValue)]
+    public static partial ReadOnlySpan<char> Char();
+
+    [return: PrimitiveStaticData(new short[] { 0, 1, -1, short.MinValue, short.MaxValue })]
+    public static partial ReadOnlySpan<short> Short();
+
+    [return: PrimitiveStaticData(new ushort[] { 1, 2, 3, ushort.MinValue, ushort.MaxValue })]
+    public static partial ReadOnlySpan<ushort> UShort();
+
+    public partial struct ChildStruct
+    {
+        public static void Write()
         {
+            Write<int>(Int());
+            Write<uint>(UInt());
+            Write<float>(Float());
         }
+
+        [return: PrimitiveStaticData(0, 1, -1, int.MinValue, int.MaxValue)]
+        public static partial ReadOnlySpan<int> Int();
+
+        [return: PrimitiveStaticData(1, 2, 3, uint.MinValue, uint.MaxValue)]
+        internal static partial ReadOnlySpan<uint> UInt();
+
+        [return: PrimitiveStaticData(0, 1, -1, float.MinValue, float.MaxValue)]
+        internal static partial ReadOnlySpan<float> Float();
+    }
+
+    protected partial class ChildClass<TDummy>
+    {
+        public static void Write()
+        {
+            Write<long>(Long());
+            Write<ulong>(ULong());
+            Write<double>(Double());
+        }
+
+        [return: PrimitiveStaticData(0, 1, -1, long.MinValue, long.MaxValue)]
+        private static partial ReadOnlySpan<long> Long();
+
+        [return: PrimitiveStaticData(1, 2, 3, ulong.MinValue, ulong.MaxValue)]
+        internal static partial ReadOnlySpan<ulong> ULong();
+
+        [return: PrimitiveStaticData(0, 1, -1, double.MinValue, double.MaxValue)]
+        protected static partial ReadOnlySpan<double> Double();
     }
 }
